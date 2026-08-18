67c6ab68e603b6de3cbc496e302190df0f07b701
zhengw-1 <zhengw1467@gmail.com>
2026-08-18 12:13:35 -0400

    Add files via upload
    Fix DailyFlow recovery cache

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = "dailyflow-v12-safari-click-fix";
-const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./order-utils.js", "./task-inline-edit-utils.js", "./icons/icon-192.png", "./icons/icon-512.png"];
+const CACHE = "dailyflow-v16-recovery-cache-sync";
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./order-utils.js?v=16", "./task-inline-edit-utils.js?v=16", "./icons/icon-192.png", "./icons/icon-512.png"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
   self.skipWaiting();
---
+++ tests/cache-version-sync.test.js
@@ -0,0 +1,19 @@
+
+const fs=require('fs');
+const path=require('path');
+
+const html=fs.readFileSync(path.join(__dirname,'..','index.html'),'utf8');
+const sw=fs.readFileSync(path.join(__dirname,'..','service-worker.js'),'utf8');
+const order=fs.readFileSync(path.join(__dirname,'..','order-utils.js'),'utf8');
+
+function assert(cond,msg){ if(!cond) throw new Error(msg); }
+
+assert(order.includes('function applyOverduePriority'), 'order-utils must contain overdue priority function');
+assert(html.includes('./order-utils.js?v=16'), 'index must cache-bust order-utils');
+assert(html.includes('./task-inline-edit-utils.js?v=16'), 'index must cache-bust inline utility');
+assert(/const CACHE\s*=\s*"dailyflow-v16-recovery-cache-sync"/.test(sw),
+  'service worker cache name must be bumped using the actual CACHE constant');
+assert(sw.includes('"./order-utils.js?v=16"'), 'service worker must cache the versioned order utility');
+assert(sw.includes('"./task-inline-edit-utils.js?v=16"'), 'service worker must cache the versioned inline utility');
+
+console.log('PASS recovery cache/version synchronization');
